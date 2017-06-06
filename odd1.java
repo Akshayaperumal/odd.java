1845aefbb52d2405a9e0c7ca8d5e10c62976f3c0
Akshayaperumal <akshayaperumal1996@gmail.com>
2017-06-06 11:25:16 +0530

    Update and rename README.md to odd1.java

---
+++ odd1.java
@@ -0,0 +1,17 @@
+# odd.java
+import java.io.*;
+import java.util.*;
+public class odd
+{
+public static void main(String args[])
+{
+int n=new int();
+if(n%2==0)
+{
+Systrm.out.println("even");
+}
+else{
+System.out.println("odd");
+}
+}
+}
